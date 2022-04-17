1051ce91a0e4f0cb3093a55ee3d1c0092053f439
Atique Morshed <iamsamix17@gmail.com>
2022-04-17 19:37:31 +0600

    Email verification implementation and show warning when email is not verified

--- src/Pages/Shared/Header/Header/Header.jsx
+++ src/Pages/Shared/Header/Header/Header.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faClose,
+  faXmark,
+  faXmarkCircle,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import CustomLink from '../CustomLink/CustomLink';
@@ -9,12 +14,38 @@ import MenuLinks from '../MenuILinks/MenuLinks';
 import HashLink from '../HashLink/HashLink';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../../Firebase/firebase.init';
-import { signOut } from 'firebase/auth';
+import { sendEmailVerification, signOut } from 'firebase/auth';
 
 const Header = () => {
   const [user] = useAuthState(auth);
 
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [emailVerificationWarning, setEmailVerificationWarning] = useState(
+    'Account not verified'
+  );
+  const [error, setError] = useState();
+
+  const [closeEmailVerificationWarning, setCloseEmailVerificationWarning] =
+    useState(false);
+
+  let errorTimeout;
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(errorTimeout);
+    };
+  }, [errorTimeout]);
+
+  const handleSendEmailVerification = async () => {
+    clearTimeout(errorTimeout);
+    try {
+      await sendEmailVerification(auth.currentUser);
+      setEmailVerificationWarning('Verification email sent!');
+    } catch (error) {
+      setError(error?.message);
+      errorTimeout = setTimeout(() => setError(), 5000);
+    }
+  };
 
   return (
     <div className="">
@@ -59,6 +90,40 @@ const Header = () => {
         <div className="nav-links flex flex-col items-center pb-4 mb-8 md:hidden w-9/12 mx-auto">
           <MenuLinks />
         </div>
+      )}
+      {!closeEmailVerificationWarning && user && !user?.emailVerified && (
+        <>
+          <div
+            className={`flex justify-center items-center gap-2 py-2 border ${
+              emailVerificationWarning === 'Verification email sent!'
+                ? 'text-blue-700'
+                : 'text-red-500'
+            }`}
+          >
+            <p className="text-sm text-center">
+              {emailVerificationWarning}{' '}
+              <span className="hidden md:inline">
+                , Please check your email.
+              </span>
+            </p>
+            <button
+              onClick={handleSendEmailVerification}
+              className="text-sm underline hover:text-gray-700 cursor-pointer"
+            >
+              Resend Email
+            </button>
+            <FontAwesomeIcon
+              className="text-gray-700 h-5 w-5 cursor-pointer hover:text-gray-500"
+              icon={faXmark}
+              onClick={() => setCloseEmailVerificationWarning(true)}
+            />
+          </div>
+          {error && (
+            <p className="text-center py-2 border text-sm text-red-500">
+              {error}
+            </p>
+          )}
+        </>
       )}
     </div>
   );
